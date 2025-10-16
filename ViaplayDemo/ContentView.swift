fba15c37e28d6dce82499bbc2a98e7dab8d0feea
Joakim Hellgren <hellojoakim@gmail.com>
2025-10-16 20:46:41 +0200

    Initial Commit

---
+++ ViaplayDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ViaplayDemo
+//
+//  Created by Joakim Hellgren on 2025-10-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ViaplayDemo/ViaplayDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViaplayDemoApp.swift
+//  ViaplayDemo
+//
+//  Created by Joakim Hellgren on 2025-10-16.
+//
+
+import SwiftUI
+
+@main
+struct ViaplayDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
